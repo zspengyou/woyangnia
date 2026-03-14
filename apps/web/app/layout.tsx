b15b161b99da083e0717fe880c51c39a048632fd
Jay Zhou <zspengyou@gmail.com>
2026-03-14 16:54:03 -0400

    feat: initial commit

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono, Figtree } from "next/font/google"
+
+import "@workspace/ui/globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@workspace/ui/lib/utils";
+
+const figtree = Figtree({subsets:['latin'],variable:'--font-sans'})
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontMono.variable, "font-sans", figtree.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
